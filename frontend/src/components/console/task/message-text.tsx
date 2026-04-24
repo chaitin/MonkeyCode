9fafcc3b6a2f507ea9d2064a7008f09cf2b47bc1
Monster <389264167@qq.com>
2026-04-24 11:45:28 +0800

    fix: expand text message width

--- frontend/src/components/console/task/message-text.tsx
+++ frontend/src/components/console/task/message-text.tsx
@@ -4,7 +4,7 @@ import { Markdown } from "@/components/common/markdown"
 
 export const TextMessageItem = ({ message }: { message: MessageType }) => {
   return (
-    <div className="flex flex-col w-fit rounded-md px-1 max-w-[100%] mt-0.5">
+    <div className="flex flex-col w-full rounded-md px-1 max-w-[100%] mt-0.5">
       <div className="text-sm">
         <Markdown allowHtml>{message.data.content || ''}</Markdown>
       </div>
